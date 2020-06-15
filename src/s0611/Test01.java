ff185bf40e7a24845be4945b9de9ffca6b342fb9
qpnnm <66932882+qpnnm@users.noreply.github.com>
2020-06-15 15:45:15 +0900

    Update Test01.java

--- src/s0611/Test01.java
+++ src/s0611/Test01.java
@@ -3,7 +3,8 @@ package s0611;
 public class Test01 {
 
    public static void main(String[] args) { 
-         System.out.println("�;�~~");
-       
+         System.out.println("¿Í¾¾~~");
+       public static void main(String[] args){
+          System.out.println("hello,world.")
    }
 }
