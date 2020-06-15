c611509c1fe3b9cbcb75912216a094e8107112a5
qpnnm <qpnnm@naver.com>
2020-06-15 15:37:16 +0900

    test

---
+++ src/s0611/Test02.java
@@ -0,0 +1,15 @@
+package s0611;
+
+public class Test02 {
+	public static void main(String[] args) {
+		byte b = 1;
+		short s = 2;
+		int i = 1111111111;
+		long l = 1111111111L;
+
+		float f = 1.1F;
+
+		double d = 2.1;
+
+	}
+}
---
+++ src/s0612/Test06.java
@@ -0,0 +1,11 @@
+package s0612;
+
+public class Test06 {
+   public static void main(String[] args) {
+	int a =1;
+	a++;
+	++a;
+	
+	
+	}
+}
