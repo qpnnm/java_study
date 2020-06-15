60061f70c05f1a17886542a373f1c2bf7f9afa57
qpnnm <qpnnm@naver.com>
2020-06-15 15:59:57 +0900

    java_study123

--- src/s0611/Test01.java
+++ src/s0611/Test01.java
@@ -3,13 +3,12 @@ package s0611;
 public class Test01 {
 
    public static void main(String[] args) { 
-<<<<<<< HEAD
+
          System.out.println("�;�~~");
-       /ttt
-=======
+
          System.out.println("¿Í¾¾~~");
-       public static void main(String[] args){
-          System.out.println("hello,world.")
->>>>>>> branch 'master' of https://github.com/qpnnm/java_study.git
+       
+          System.out.println("hello,world.");
+
    }
 }
